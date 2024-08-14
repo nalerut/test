58c4043c9cc6d961378d5df6c1844d511d450792
Rutuja Nale <rutuja.nale@msci.com>
2024-08-14 11:09:19 +0530

    build

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+// Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://733306be114b3375ddeb02b1330d0281@o4507751453622272.ingest.de.sentry.io/4507774021599312",
+  integrations: [new Sentry.BrowserTracing({}), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
